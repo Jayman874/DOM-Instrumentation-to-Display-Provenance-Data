9631a74115f9b81e7dbef5c1bfee3aa04ad67aa6
Jayen <gulabjaye@myvuw.ac.nz>
2023-05-18 09:05:34 +1200

    Added list of Headers that have manipulated the DOM to the webpage.

--- Browser Plugin/XMLHttpRequest DOM Manipulation Detector/background.js
+++ Browser Plugin/XMLHttpRequest DOM Manipulation Detector/background.js
@@ -1,7 +1,7 @@
 function responseUrls(details) {
   if (details.type != null && details.method != null && details.requestId != null){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, details.url);
+      chrome.tabs.sendMessage(tabs[0].id, details);
     });
   }
 }
--- Browser Plugin/XMLHttpRequest DOM Manipulation Detector/main.js
+++ Browser Plugin/XMLHttpRequest DOM Manipulation Detector/main.js
@@ -3,6 +3,7 @@ console.log("The extension is up and running");
 let scriptPagesXML = [];
 let url = {};
 
+// gets all scripts within webpage that use XMLHttpRequest
 function getXMLHttpRequest(){
     let page = document.getElementsByTagName("script");
     for (i = 0; i < page.length; i++){
@@ -16,6 +17,7 @@ function getXMLHttpRequest(){
     }
 }
 
+// highlights the DOM element which has been manipulated by an Http Request
 function highlightDOM(domElement){
     let startIndex = domElement.indexOf("document.getElementById(");
     let sub = domElement.substring(startIndex);
@@ -25,6 +27,7 @@ function highlightDOM(domElement){
     document.getElementById(final).style.background = "red";
 }
 
+// gets the url of all requests used in XMLHttpRequests
 function getURL(scriptPagesXML){
     let startIndex = scriptPagesXML.innerText.indexOf(".open(");
     let sub = scriptPagesXML.innerText.substring(startIndex);
@@ -39,14 +42,31 @@ function getURL(scriptPagesXML){
     return final;
 }
 
+// Injects list of headers that have manipulated DOM elements
+function update() {
+    let b = document.createElement('br');
+    let details = document.createElement('details');
+    let summary = document.createElement('summary');
+    details.setAttribute("id", "stats");
+    summary.innerText = "Headers that Manipulated DOM Elements on Webpage";
+    document.body.append(b);
+    document.body.appendChild(details).appendChild(summary);
+    document.getElementById("stats").style.fontSize ="x-small";
+    getXMLHttpRequest();
+}
+
+// receives all headers entering webpage and see if any match urls being used in the DOM
 chrome.runtime.onMessage.addListener(function(request, sendResponse) {
+    let doc = document.getElementById("stats");
     if(request.ping) { sendResponse({pong: true}); return; }
     for (let key in url){
-        if (request.includes(key)){
+        if (request.url.includes(key)){
+            doc.innerHTML += "<pre>" + JSON.stringify(request, null, 2) + "</pre>";
             highlightDOM(url[key]);
             return;
         }
     }
 });
 
-getXMLHttpRequest();
+update();
+
