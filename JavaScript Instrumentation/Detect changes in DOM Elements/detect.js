d70215ff4cbf94dbab51fd50f24e862b1b9520e9
Jayen <gulabjaye@myvuw.ac.nz>
2023-06-28 20:49:56 +1200

    Added Pure Javscript implementation where the DOM is instrumted to return and dertermine whether an object has been updated through ajax. This so far works for XMLHttpRequest and Ajax.

--- JavaScript Instrumentation/Detect changes in DOM Elements/detect.js
+++ JavaScript Instrumentation/Detect changes in DOM Elements/detect.js
@@ -1,10 +1,14 @@
+let colours = ["red", "blue", "green", "yellow", "purple", "orange", "cyan", "violet", "turquoise", "grey"];
+
 let observer = new MutationObserver((mutations) => {
   mutations.forEach(async (mutation) => {
     let oldValue = mutation.oldValue;
     let newValue = mutation.target;
     if (oldValue !== newValue) {
-      let provenance = "\n" + mutation.target.provenance;
-      console.trace(provenance);
+      let provenance = newValue.provenance;
+      if (provenance != undefined && newValue.id != "stats"){
+        provenanceString(mutation);
+      }
     }
   });
 });
@@ -15,3 +19,24 @@ observer.observe(document.body, {
   childList: true, 
   characterData: true
 });
+
+function provenanceString(newValue){
+  const randomElement = colours[Math.floor(Math.random() * colours.length)];
+  const index = colours.indexOf(randomElement);
+  if (index >= -1){
+    colours.splice(index, 1);
+  }
+  let doc = document.getElementById("stats");
+  let prov = "DOM text content: (" + newValue.removedNodes[0].textContent + ")\n" +
+  "updated to: (" + newValue.target.innerHTML + ")\n" +
+  "at DOM id: (" + newValue.target.id + ")\n" +
+  "from the url: (" + newValue.target.url + ")\n" +
+  "responding with the headers of: \n" + newValue.target.provenance + "\n" +
+  "This element has been highlighted on the webpage as the color:"; 
+  doc.innerHTML += "<pre>" + prov + '<div style="background-color:' + randomElement + ';>""</div> </pre>';
+  highlightDOM(newValue.target.id, randomElement);
+}
+
+function highlightDOM(element, colour) {
+  document.getElementById(element).style.background = colour;
+}
--- JavaScript Instrumentation/Detect changes in DOM Elements/inject.js
+++ JavaScript Instrumentation/Detect changes in DOM Elements/inject.js
@@ -1,12 +1,30 @@
 function update() {
-    let b = document.createElement('br');
-    let details = document.createElement('details');
-    let summary = document.createElement('summary');
-    details.setAttribute("id", "stats");
-    summary.innerText = "Changes in DOM";
-    document.body.append(b);
-    document.body.appendChild(details).appendChild(summary);
-    document.getElementById("stats").style.fontSize ="x-small";
+  var scripts = document.getElementsByTagName('script');
+  var text = "document.getElementById(";
+  for (let i = 0; i < scripts.length; i++){
+    if (scripts[i].innerText.includes("new XMLHttpRequest()") || scripts[i].innerText.includes("$.ajax")){
+      var parts = scripts[i].innerText.split(text);
+      var index = parts.slice(1).toString().split(")");
+      var codeURL = "document.getElementById(" + index[0] + ").url = xhr.responseURL; \n";
+      var codeHeaders = "document.getElementById(" + index[0] + ").provenance = xhr.getAllResponseHeaders(); \n";
+      var newText = parts[0] + codeURL + codeHeaders + text + parts.slice(1).join(text);
+      var newScriptElement = document.createElement('script');
+      newScriptElement.append(newText);
+      scripts[i].parentNode.replaceChild(newScriptElement, scripts[i]);
+    }
+  }
+}
+
+function add() {
+  let b = document.createElement('br');
+  let details = document.createElement('details');
+  let summary = document.createElement('summary');
+  details.setAttribute("id", "stats");
+  summary.innerText = "DOM Elements Manipulated by Ajax";
+  document.body.append(b);
+  document.body.appendChild(details).appendChild(summary);
+  document.getElementById("stats").style.fontSize ="x-small";
 }
 
 update();
+add();
