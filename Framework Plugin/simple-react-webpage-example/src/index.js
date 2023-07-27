c88ed94456f3b5427082ef8d2a89c53033000f88
Jayen <gulabjaye@myvuw.ac.nz>
2023-07-27 19:56:44 +1200

    Configured Plugins so they can be published on the node package manager.

--- Framework Plugin/simple-react-webpage-example/src/index.js
+++ Framework Plugin/simple-react-webpage-example/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Instrument from './instrument.js';
 import reportWebVitals from './reportWebVitals';
+import Instrument from 'ajax-instrumentation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
